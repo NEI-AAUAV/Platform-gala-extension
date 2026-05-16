099c56150190b93f826642efe7c275f2184c7c32
Gabriel Silva <gabrielmsilva4@ua.pt>
2026-05-16 16:35:55 +0100

    fix(time-slots): make GET /time public, no auth required

--- api-gala/app/api/time_slots/get.py
+++ api-gala/app/api/time_slots/get.py
@@ -1,23 +1,14 @@
-from typing import Annotated
-from fastapi import APIRouter, Security
+from fastapi import APIRouter
 
 from app.models.time_slots import TimeSlots
 from app.core.db import DatabaseDep
-from app.api.auth import AuthData, api_nei_auth, auth_responses
 
 from .util import fetch_time_slots
 
 router = APIRouter()
 
 
-@router.get(
-    "/",
-    responses={**auth_responses},
-)
-async def get_time_slots(
-    *,
-    db: DatabaseDep,
-    _: Annotated[AuthData, Security(api_nei_auth)],
-) -> TimeSlots:
+@router.get("/")
+async def get_time_slots(*, db: DatabaseDep) -> TimeSlots:
     """Gets the current time slots config"""
     return await fetch_time_slots(db)
